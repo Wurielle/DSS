c0fc36093b6c379a391398d7548f0576ee91d986
Wurielle <noahfenghom@gmail.com>
2018-12-07 18:26:59 +0100

    Added Detector to custom-parser

--- test/test-custom-parser.js
+++ test/test-custom-parser.js
@@ -3,17 +3,38 @@ var fs = require('fs');
 var path = require('path');
 
 exports.testParse = function(test){
-    test.expect(2);
+    test.expect(3);
 
     var fileContents = fs.readFileSync(path.join(__dirname, 'data/custom-parser.css'), 'utf8');
+    const detectorFilter = [
+        'media',
+        'import',
+        'tailwind',
+        'apply',
+        'variants',
+        'responsive',
+        'screen'
+    ];
+    const match = new RegExp(".*@");
+    const unmatch = new RegExp(".*@(" + detectorFilter.join("|") + ")");
+    dss.detector( function(line) {
+        if (typeof line !== 'string') {
+            return false;
+        }
+        const reference = line.split("\n\n").pop();
+        return !!reference.match(match) && !reference.match(unmatch);
+    });
+    
     dss.parser('sectionmaker', function () {
         // Just returns the lines contents
         return this.line.contents.split('>').map(section => section.trim());
     });
+
     dss.parse(fileContents, {}, function(parsed) {
         console.log(JSON.stringify(parsed, null, 2));
         var block = parsed.blocks[0];
         test.equal(block.helloworld, 'hello world');
+        test.equal(block.media, undefined);
         test.equal(JSON.stringify(block.sectionmaker), JSON.stringify(['Base', 'Button']));
         test.done();
     });
